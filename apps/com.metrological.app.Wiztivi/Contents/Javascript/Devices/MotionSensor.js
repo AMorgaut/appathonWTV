262ebfd3797ece43014920bee6905c92b653dee7
gqueste <gabriel.queste@wiztivi.com>
2017-09-19 23:25:47 +0200

    MotionSensor

---
+++ apps/com.metrological.app.Wiztivi/Contents/Javascript/Devices/MotionSensor.js
@@ -0,0 +1,22 @@
+var active = false;
+
+var MotionSensor = {
+    setActive: function(bool) {
+        active = bool;
+    },
+    
+    getActive: function() {
+        return active;    
+    },
+
+    reactOn: function(reactCallback) {
+        var myFunction = function() {
+            Requests.doRequest("GET", "http://the-thing.appathon.tv/philips-hue/api/59MSn7uqdUFj96Gb0r7fevecVH9A5IRSbEonz7va/sensors/7", reactCallback);
+            if (active) {
+                setTimeout(myFunction, 1000);
+            }
+        };
+        myFunction();
+    }
+
+};
