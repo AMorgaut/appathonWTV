f916d7c124f7c9b84e324c0f5a1871b08fe9b710
gqueste <gabriel.queste@wiztivi.com>
2017-09-19 21:30:12 +0200

    Devices list

--- apps/com.metrological.app.Wiztivi/Contents/Javascript/init.js
+++ apps/com.metrological.app.Wiztivi/Contents/Javascript/init.js
@@ -3,6 +3,7 @@ include( 'Javascript/Sections.js');
 include( 'Javascript/Scenarios.js');
 
 include( 'Javascript/Services/Requests.js');
+include( 'Javascript/Devices/Devices.js');
 include( 'Javascript/Devices/Fan.js');
 include( 'Javascript/Devices/Door.js');
 include( 'Javascript/Devices/Curtains.js');
