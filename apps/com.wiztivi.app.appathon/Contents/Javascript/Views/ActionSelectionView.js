ac9470028518f6d4b873a27656e27e9db8173777
Thomas ANDRE <tandre@mdevlnx023.lan.wiztivi.com>
2017-09-20 12:13:22 +0200

    proper version without background images

--- apps/com.wiztivi.app.appathon/Contents/Javascript/Views/ActionSelectionView.js
+++ apps/com.wiztivi.app.appathon/Contents/Javascript/Views/ActionSelectionView.js
@@ -60,7 +60,7 @@ var ActionSelectionView = new MAF.Class( {
 
                             this.setStyle(
                                 'backgroundColor',
-                                'red'
+                                '#01A0E2'
                             );
                             selectedCell = this.getCellDataIndex();
                         },
