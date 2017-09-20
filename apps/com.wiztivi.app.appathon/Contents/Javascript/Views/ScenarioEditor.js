89b49890ce7b1f8cbf5dd28c1e9078779716799f
Thomas ANDRE <tandre@mdevlnx023.lan.wiztivi.com>
2017-09-20 13:33:52 +0200

    RAF

--- apps/com.wiztivi.app.appathon/Contents/Javascript/Views/ScenarioEditor.js
+++ apps/com.wiztivi.app.appathon/Contents/Javascript/Views/ScenarioEditor.js
@@ -1,6 +1,20 @@
 var selectedCell = undefined;
 var sliderGrid = undefined;
 var selectedDevice = undefined;
+var savedValue = 0;
+
+var imagesValues = [
+    'Images/forestCurtains.png',
+    'Images/philipsMotionIcon.png',
+    'Images/sonosPlay1Icon.png',
+    'Images/doorbirdIcon.png',
+    'Images/hueIcon.png',
+    'Images/nukiLockIcon.png',
+    'Images/dysonFan.png',
+    'Images/siemensCoffeeIcon.png',
+    'Images/netgearArloIcon.png',
+    'Images/siemensFridgeIcon.png'
+];
 
 var ScenarioEditor = new MAF.Class( {
     Classname: 'ScenarioEditor',
@@ -82,16 +96,32 @@ var ScenarioEditor = new MAF.Class( {
                         color: '#F1F1F1',
                         fontSize: 32
                     }
-                } ).appendTo( cell );
+                }).appendTo(cell);
+                cell.icon = new MAF.element.Image({
+                    styles: {
+                        width: 100,
+                        height: 100,
+                        hOffset: (cell.width - 100) / 2,
+                        vOffset: (cell.height - 250) / 2
+                        //backgroundImage: imagesValues[0]
+                    },
+                    src: imagesValues[savedValue]
+                }).appendTo(cell);
+                savedValue++;
                 return cell;
             },
 
             cellUpdater: function( cell, data ) {
+
                 cell.title.setText( data.title );
+
+                console.log(cell);
+                console.log(data);
+                cell.icon.src = data.src;
             },
             events: {
                     onDatasetChanged: function() {
-                    this.getCurrentCell().focus();
+                    //this.getCurrentCell().focus();
                 }
             }
         }).appendTo( this );
@@ -121,16 +151,46 @@ var ScenarioEditor = new MAF.Class( {
 
     focusView: function() {
         this.elements.slider.changeDataset( [
-            { title: $_( 'Forest Curtains' ) },
-            { title: $_( 'Philips Motion Sensor' ) },
-            { title: $_( 'Sonos PLAY:1 Black' ) },
-            { title: $_( 'Doorbird RVS' ) },
-            { title: $_( 'Philips Hue' ) },
-            { title: $_( 'Nuki Smart Lock' ) },
-            { title: $_( 'Dyson Pure Hot&Cool' ) },
-            { title: $_( 'Siemens Coffemachine' ) },
-            { title: $_( 'Netgear Arlo Pro Duo' ) },
-            { title: $_( 'Siemens Fridge iQ500' ) }
+            {
+                title: $_( 'Forest Curtains' ),
+                src: 'Images/forestCurtains.png'
+            },
+            {
+                title: $_( 'Philips Motion Sensor' ),
+                src: 'Images/philipsMotionIcon.png'
+            },
+            {
+                title: $_( 'Sonos Play 1' ),
+                src: 'Images/sonosPlay1Icon.png'
+            },
+            {
+                title: $_( 'Doorbird RVS' ),
+                src: 'Images/doorbirdIcon.png'
+            },
+            {
+                title: $_( 'Philips Hue' ),
+                src: 'Images/hueIcon.png'
+            },
+            {
+                title: $_( 'Nuki Smart Lock' ),
+                src: 'Images/nukiLockIcon.png'
+            },
+            {
+                title: $_( 'Dyson Pure Hot&Cool' ),
+                src: 'Images/dysonFan.png'
+            },
+            {
+                title: $_( 'Siemens Coffemachine' ),
+                src: 'Images/siemensCoffeeIcon.png'
+            },
+            {
+                title: $_( 'Netgear Arlo Pro Duo' ),
+                src: 'Images/netgearArloIcon.png'
+            },
+            {
+                title: $_( 'Siemens iQ500' ),
+                src: 'Images/siemensFridgeIcon.png'
+            }
         ], true );
     }
 });
